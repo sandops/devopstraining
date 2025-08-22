1e6d91cd577f6f9da7e3834b6146c5bb5f19935f
Sandeep <sandeep.patel536@gmail.com>
2025-08-22 07:11:17 +0000

    Commit for new_1.py

---
+++ new_1.py
@@ -0,0 +1,19 @@
+import json
+print(f"This is new world")
+name = input(f"Enter your name: ")
+print(f"Hello {name} welcome to this spectacular language")
+name_file = "/home/ubuntu/gitdemo/json_file"
+with open (name_file, 'w') as wr:
+    json.dump(name,wr)
+
+with open (name_file , 'r') as f:
+   username = json.load(f)
+   print(f"Hi {username} welcome back")
+print("This is the end of with block")
+print("This is end of file")
+motorcycles = ['Ducati', 'Suzuki', 'Honda', 'Hero']
+print(f"\nHi {username},please checkout the motorbikes brands you can choose from: \n")
+print("\n"+ motorcycles[0]+ "\n")
+print("\n"+ motorcycles[1] + "\n")
+print('\n'+  motorcycles[2] + '\n')
+print('\n'+ motorcycles[3] + '\n')
