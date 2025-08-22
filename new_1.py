43509b56a24871104ebb47c60031ba486ec9fae6
Sandeep <sandeep.patel536@gmail.com>
2025-08-22 08:09:57 +0000

    new_1.py file commit in feature1 branch

--- new_1.py
+++ new_1.py
@@ -11,9 +11,11 @@ with open (name_file , 'r') as f:
    print(f"Hi {username} welcome back")
 print("This is the end of with block")
 print("This is end of file")
-motorcycles = ['Ducati', 'Suzuki', 'Honda', 'Hero']
+motorcycles = ['Ducati', 'Suzuki', 'Honda', 'Hero', 'Royal Enfield']
 print(f"\nHi {username},please checkout the motorbikes brands you can choose from: \n")
 print("\n"+ motorcycles[0]+ "\n")
 print("\n"+ motorcycles[1] + "\n")
 print('\n'+  motorcycles[2] + '\n')
 print('\n'+ motorcycles[3] + '\n')
+print('\n' + motorcycles[5] + '\n')
+
